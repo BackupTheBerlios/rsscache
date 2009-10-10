7cae0690d534d240ceaf89ef9d40661b31db03b5
root <root>
2009-10-10 08:10:55 +0000

    *** empty log message ***

--- rsscache/htdocs/index.php
+++ rsscache/htdocs/index.php
@@ -2,10 +2,6 @@
 //phpinfo();
 //error_reporting(E_ALL | E_STRICT);
 
-if (!strncmp ($_SERVER['HTTP_HOST'], 'demos.', 6) ||
-    !strncmp ($_SERVER['SERVER_NAME'], 'demos.', 6))
-  require_once ('pwnoogle_demos.php');
-else
-  require_once ('tv2.php');
+require_once ('tv2.php');
 
 ?>
