72090d2c462818f920699613c6b5fea3edfad285
root <root>
2010-04-06 16:28:23 +0000

    *** empty log message ***

--- rsscache/htdocs/config.php
+++ rsscache/htdocs/config.php
@@ -14,7 +14,10 @@ $memcache_expire = 0;
 //$tv2_charset = 'utf-8';
 $tv2_search_s = 'Search';
 $tv2_title = 'pwnoogle \:D/ - watch videos of games';
-$tv2_body_tag = '<body style="font-family:sans-serif;font-size:13px;opacity:1.0;">';
+$tv2_body_tag = '<body style="font-family:sans-serif;font-size:13px;opacity:1.0;">'
+               .'<a href="http://pwnoogle.com/demos.php">Videos</a>&nbsp;'
+               .'<a href="http://demos.pwnoogle.com/demos.php">Demos</a>&nbsp;'
+               .'<a href="http://clanarena.org"><img src="images/ca.png" border="0" height="32"></a><br>';
 $tv2_table_tag = '<table style="font-family:sans-serif;font-size:13px;opacity:1.0;" cellspacing="0" cellpadding="0" border="0">';
 $tv2_logo = '<span style="font-size:32px;font-family:sans-serif;color:#000;font-weight:bolder;width:100%;text-align:center">'
            .'pwnoogle \:D/'
