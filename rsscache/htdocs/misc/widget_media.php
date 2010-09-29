6e0f7d691bd8201d63c38f21bae7a5e8ed55594f
root <root>
2010-09-29 15:54:30 +0000

    *** empty log message ***

--- rsscache/htdocs/misc/widget_media.php
+++ rsscache/htdocs/misc/widget_media.php
@@ -270,105 +270,7 @@ widget_video_youtube ($video_url, $width = 425, $height = 344, $download = 0, $a
     $video_url = substr ($video_url, strpos ($video_url, 'watch') + 12);
   $video_url = str_replace ('&feature=youtube_gdata', '', $video_url);
 
-/*
-rel
-    Values: 0 or 1. Default is 1. Sets whether the player should load
-related videos once playback of the initial video starts.  Related videos
-are displayed in the "genie menu" when the menu button is pressed.  The
-player search functionality will be disabled if rel is set to 0.
-
-autoplay
-    Values: 0 or 1. Default is 0. Sets whether or not the initial video will
-autoplay when the player loads.
-
-loop
-    Values: 0 or 1. Default is 0. In the case of a single video player, a
-setting of 1 will cause the player to play the initial video again and
-again.  In the case of a playlist player (or custom player), the player will
-play the entire playlist and then start again at the first video.
-
-enablejsapi
-    Values: 0 or 1. Default is 0. Setting this to 1 will enable the
-Javascript API.  For more information on the Javascript API and how to use
-it, see the JavaScript API documentation.
-
-playerapiid
-    Value can be any alphanumeric string. This setting is used in
-conjunction with the JavaScript API.  See the JavaScript API documentation
-for details.
-
-disablekb
-    Values: 0 or 1. Default is 0. Setting to 1 will disable the player
-keyboard controls.  Keyboard controls are as follows:
-         Spacebar: Play / Pause
-         Arrow Left: Jump back 10% in the current video
-         Arrow Right: Jump ahead 10% in the current video
-         Arrow Up: Volume up
-         Arrow Down: Volume Down 
-egm
-    Values: 0 or 1. Default is 0. Setting to 1 enables the "Enhanced Genie
-Menu".  This behavior causes the genie menu (if present) to appear when the
-user's mouse enters the video display area, as opposed to only appearing
-when the menu button is pressed.
-
-border
-    Values: 0 or 1. Default is 0. Setting to 1 enables a border around the
-entire video player.  The border's primary color can be set via the color1
-parameter, and a secondary color can be set by the color2 parameter.
-
-color1, color2
-    Values: Any RGB value in hexadecimal format. color1 is the primary
-border color, and color2 is the video control bar background color and
-secondary border color.
-
-start
-    Values: A positive integer. This parameter causes the player to begin
-playing the video at the given number of seconds from the start of the
-video.  Note that similar to the seekTo function, the player will look for
-the closest keyframe to the time you specify.  This means sometimes the play
-head may seek to just before the requested time, usually no more than ~2
-seconds.
-
-fs
-    Values: 0 or 1. Default is 0. Setting to 1 enables the fullscreen
-button.  This has no effect on the Chromeless Player.  Note that you must
-include some extra arguments to your embed code for this to work.  The
-bolded parts of the below example enable fullscreen functionality:
-
-    <param name="allowFullScreen" value="true"></param>
-    <embed       allowfullscreen="true">
-    </embed>
-
-hd
-    Values: 0 or 1. Default is 0. Setting to 1 enables HD playback by
-default.  This has no effect on the Chromeless Player.  This also has no
-effect if an HD version of the video is not available.  If you enable this
-option, keep in mind that users with a slower connection may have an
-sub-optimal experience unless they turn off HD.  You should ensure your
-player is large enough to display the video in its native resolution.
-
-showsearch
-    Values: 0 or 1. Default is 1. Setting to 0 disables the search box from
-displaying when the video is minimized.  Note that if the rel parameter is
-set to 0 then the search box will also be disabled, regardless of the value
-of showsearch.
-
-showinfo
-    Values: 0 or 1. Default is 1. Setting to 0 causes the player to not
-display information like the video title and rating before the video starts
-playing.
-
-iv_load_policy
-    Values: 1 or 3. Default is 1. Setting to 1 will cause video annotations
-to be shown by default, whereas setting to 3 will cause video annotation to
-not be shown by default.
-
-cc_load_policy
-    Values: 1. Default is based on user preference. Setting to 1 will cause
-closed captions to be shown by default, even if the user has turned captions
-off.
-*/
-      
+//http://code.google.com/apis/youtube/player_parameters.html      
   $url = 'http://www.youtube.com/v/'
         .$video_url
        .'&fs=1'             // allow fullscreen
@@ -766,6 +668,36 @@ widget_video_yahoo ($video_url, $width=512, $height=322, $download = 0, $autopla
 
 
 function
+widget_video_owned ($video_url, $width=640, $height=360, $download = 0, $autoplay = 0, $hq = 0, $loop = 0)
+{
+  //http://www.own3d.tv/video/25617
+  //http://www.own3d.tv/stream/25617
+  $url = str_replace ('watch', 'stream', $video_url);
+
+  $o = array (
+    array ('width', $width),
+    array ('height', $height),
+  );
+  $p = array (
+    array ('movie', $url),
+    array ('allowFullScreen', 'true'),
+    array ('allowScriptAccess', 'always'),
+    array ('wmode', 'transparent'),
+  );
+  $e = array (
+    array ('src', $url),
+    array ('type', 'application/x-shockwave-flash'),
+    array ('width', $width),
+    array ('height', $height),
+    array ('allowFullScreen', 'true'),
+    array ('allowScriptAccess', 'always'),
+    array ('wmode', 'transparent'),
+  );
+  return widget_media_object_func ($o, $p, $e);
+}
+
+
+function
 widget_media_demux ($media_url)
 {
   if (strstr ($media_url, '.youtube.com'))
@@ -793,6 +725,8 @@ widget_media_demux ($media_url)
   else if (strstr ($media_url, 'http://') && 
            in_array (strtolower (get_suffix ($media_url)), array ('.m3u', '.pls', '.xspf', '.wpl')))
     return 11; // playlist files
+  else if (strstr ($media_url, 'owned.tv'))
+    return 12;
 
   return 0;
 }
@@ -829,7 +763,8 @@ widget_media ($media_url, $width = NULL, $height = NULL, $download = 0, $autopla
          'widget_video_xvideos',
          'widget_video_xxxbunker',
          'widget_video_google',
-         'widget_video_tnaflix'
+         'widget_video_tnaflix',
+         'widget_video_owned',
 );
 
   if ($demux > 0)
