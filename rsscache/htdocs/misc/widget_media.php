f8a64e487903f44c918726f02b82f19558f3833b
root <root>
2010-11-01 19:35:59 +0000

    *** empty log message ***

--- rsscache/htdocs/misc/widget_media.php
+++ rsscache/htdocs/misc/widget_media.php
@@ -899,6 +899,25 @@ widget_media_download ($media_url, $tor_enabled = 0)
 
 
 function
+widget_video_youtubereloaded_playlist 
+{
+/*
+<script type="text/javascript" src="http://www.youtubereloaded.com/embed/swfobject.js"></script>
+<div id="YouTubeReloadedPlayer">This div will be replaced</div>
+
+<script type="text/javascript">
+var s1 = new SWFObject('http://www.youtubereloaded.com/embed/player.swf','ply','470','470','9','#');
+s1.addParam('allowfullscreen','true');
+s1.addParam('allowscriptaccess','always');
+s1.addParam('wmode','opaque');
+s1.addParam('flashvars','file=http%3A%2F%2Fgdata.youtube.com%2Ffeeds%2Fapi%2Fvideos%3Fvq%3Dcaptain+future%26max-results%3D10&playlist=bottom&frontcolor=cccccc&lightcolor=66cc00&skin=http://www.youtubereloaded.com/embed/skin1.swf&backcolor=111111&playlistsize=200');
+s1.write('YouTubeReloadedPlayer');
+</script>
+*/
+}
+
+
+function
 widget_video_youtube_playlist ($video_urls, $width = 425, $height = 344, $ratio = NULL, $autoplay = 0, $hq = 0, $loop = 0)
 {
   $p = '';
