bd109ce0176fa970526ddb733da1d8384c868b5c
root <root>
2011-02-08 05:39:50 +0000

    *** empty log message ***

--- rsscache/htdocs/misc/embed.php
+++ rsscache/htdocs/misc/embed.php
@@ -169,7 +169,7 @@ widget_embed_iframe ($src)
   // automatic scale to the content size requires javascript and misc.js
   $p = '';
 
-  $p .= '<a href='.$url.'><img src="images/widget/redirectltr.png" border="0">Open Frame in New Window</a><br>';
+  $p .= '<a href='.$url.' target="_blank"><img src="images/widget/redirectltr.png" border="0">Open Frame in New Window</a><br>';
   $p .= '<iframe'
 //       .' onload="javascript:autoscaleiframe(this);" scrolling="no"' // with js
        .' width="100%"'
