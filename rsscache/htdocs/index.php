9c7003cddfff3e88095f8913221d5b173b3c50d4
pwnoogle <pwnoogle>
2011-02-17 16:41:19 +0000

    *** empty log message ***

--- rsscache/htdocs/index.php
+++ rsscache/htdocs/index.php
@@ -1,7 +1,8 @@
 <?php
 //phpinfo();
 //error_reporting(E_ALL | E_STRICT);
+  header ('Refresh: 1; url=http://emulive.pwnoogle.com', false); // 301 moved permanently
 
-require_once ('tv2/tv2.php');
+//require_once ('tv2/tv2.php');
 
 ?>
