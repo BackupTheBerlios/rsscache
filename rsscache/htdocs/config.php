a7ceba02ef077ef335283c649ac87f2416905045
root <root>
2011-05-15 05:05:42 +0000

    *** empty log message ***

--- rsscache/htdocs/config.php
+++ rsscache/htdocs/config.php
@@ -80,6 +80,17 @@ $tv2_include_php = 'videos_include.php';
 $tv2_thumbnails_prefix = '';
   
 
+
+  $tv2_videos_s = 'videos';
+$tv2_related_s = 'Find related '.$tv2_videos_s;
+
+$tv2_enable_search = 0; // allow users to search db?
+$tv2_link = 'http://videos.pwnoogle.com/';
+$tv2_link_static = 'http://videos.pwnoogle.com/'; // static content
+$tv2_debug_sql = 0;
+$tv2_related_search = 0; // make use of related searches (requires keywords)
+
+
 if ($tv2_subdomain == 'videos' || $tv2_subdomain == ''  || $tv2_subdomain == 'www' ||
     $_SERVER['SERVER_NAME'] == 'pwnoogle.com')
 {
@@ -107,16 +118,81 @@ else if ($tv2_subdomain == 'counterstrike')
   $tv2_default_category = 'css';
   $tv2_collapsed = 1;   // collapse categories
 }
+else if ($tv2_subdomain == 'lifestyle');
+{
 
-  $tv2_videos_s = 'videos';
+// localization and style
+//$tv2_charset = 'utf-8';
+$tv2_search_s = 'Search';
+$tv2_title = 'lifestyle';
+$tv2_body_tag = '<body>';
+$tv2_logo = 'lifestyle <img src="http://plugins.pwnoogle.com/qrcode.php?data=http://lifestyle.pwnoogle.com&size=2" style="vertical-align:top;">';
+$tv2_videos_s = 'items';
 $tv2_related_s = 'Find related '.$tv2_videos_s;
+$tv2_default_category = '';  // default category
+$tv2_collapsed = 0;   // collapse categories
+$tv2_config_xml = 'config.xml';
+$tv2_include_php = 'tv2_include.php';
+$tv2_thumbnails_prefix = '';
 
-$tv2_enable_search = 0; // allow users to search db?
-$tv2_link = 'http://videos.pwnoogle.com/';
-$tv2_link_static = 'http://videos.pwnoogle.com/'; // static content
+
+$tv2_root = dirname(__FILE__).'/';
+$tv2_rss_head = 1;
+$tv2_link = 'http://lifestyle.a1.25u.com/';
+$tv2_link_static = 'http://lifestyle.a1.25u.com/'; // remote static content
+$tv2_cookie_expire = time() + 3600 * 24 * 180; // 6 months
+// DEBUG (show the SQL query)
 $tv2_debug_sql = 0;
-$tv2_related_search = 0; // make use of related searches (requires keywords)
+$tv2_isnew = 3600 * 6; // how long new files are marked as new
+$tv2_results = 10; // results per page
+$tv2_cloud_results = 500;  // number of tumbnails shown in cloud
+$tv2_wall_results = 500;  // number of tumbnails shown in wall
+$tv2_download_video = 0; // show link for downloading videos
 
+
+// player settings
+//$tv2_player_w = 400; // max. width
+//$tv2_player_h = 300; // max. height
+//$tv2_player_w = -1; $tv2_player_h = -1; // scaled
+$tv2_player_w = 640; // max. width
+$tv2_player_h = 480; // max. height
+$tv2_player_ratio = 4/3; // default ratio of player
+$tv2_preview_w = 400;  
+$tv2_preview_h = 300;
+
+
+// database settings
+$tv2_use_database = 1;
+$tv2_dbprefix = '';
+$tv2_dbhost = 'localhost';
+$tv2_dbname = 'pwnoogle_lifestyle'; 
+if ($tv2_domain == 'debian2' || $tv2_domain == 'vps24110')
+{
+  $tv2_dbuser = 'root';
+  $tv2_dbpass = 'nb';
+}
+else
+{
+  $tv2_dbuser = 'pwnoogle_db';
+  $tv2_dbpass = 'pwn44553';
+}
+
+
+$tv2_config_xml = 'lifestyle_config.xml';
+$tv2_include_php = 'tv2_include.php';
+$tv2_thumbnails_prefix = '';
+
+$tv2_title = 'lifestyle - pwnoogle \:D/';
+$tv2_logo = 'lifestyle <img src="http://plugins.pwnoogle.com/qrcode.php?data=http://lifestyle.pwnoogle.com&size=2" style="vertical-align:top;">';
+
+$tv2_link = 'http://lifestyle.pwnoogle.com/';
+$tv2_link_static = 'http://lifestyle.pwnoogle.com/'; // remote static content
+$tv2_debug_sql = 0;
+$tv2_enable_search = 1; // allow users to search db?
+$tv2_collapsed = 2; // collapse categories? 0 == no, 1 == yes, 2 == never
+
+
+}
 
 }
 
