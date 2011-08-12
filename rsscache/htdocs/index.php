5a7bc6760228ecb9441678d0efd0796f9e7dc9d8
root <root>
2011-08-12 03:04:40 +0000

    *** empty log message ***

--- rsscache/htdocs/index.php
+++ rsscache/htdocs/index.php
@@ -3,5 +3,5 @@
 //error_reporting(E_ALL | E_STRICT);
 //  header ('Refresh: 1; url=http://emulive.pwnoogle.com', false); // 301 moved permanently
 //exit;
-require_once ('rsscache.php');
+require_once ('rsscache/rsscache.php');
 ?>
