ff3d3c259a6f82d379ed71dfb922dc54271508d2
root <root>
2011-08-13 00:52:22 +0000

    *** empty log message ***

---
+++ rsscache/htdocs/admin.php
@@ -0,0 +1,8 @@
+<?php
+//phpinfo();
+//error_reporting(E_ALL | E_STRICT);
+//  header ('Refresh: 1; url=http://emulive.pwnoogle.com', false); // 301 moved permanently
+//exit;
+$rsscache_admin = 1;
+require_once ('rsscache/rsscache.php');
+?>
