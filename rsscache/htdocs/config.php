e8c70612ab9f58f39e36ce081745571505775b55
root <root>
2011-08-14 05:16:14 +0000

    *** empty log message ***

--- rsscache/htdocs/config.php
+++ rsscache/htdocs/config.php
@@ -1,44 +1,92 @@
 <?php
-if (!defined ('RSSCACHE_CONFIG_PHP'))
+if (!defined ('_TV2_CONFIG_PHP'))
 {
-define ('RSSCACHE_CONFIG_PHP', 1);
+define ('_TV2_CONFIG_PHP', 1);
+
+// apache2
+//$_SERVER['SERVER_NAME'] == 'maps.pwnoogle.com'
+//$_SERVER['USER'] unset
+//$_SERVER["HOSTNAME"] => unset
+//$_SERVER["PWD"] => unset
+//$_SERVER["DOCUMENT_ROOT"] => /home/pwnoogle/htdocs/emulive/htdocs
+
+// cmdline
+//$_SERVER['SERVER_NAME'] unset
+//$_SERVER['USER'] == 'pwnoogle'
+//$_SERVER["HOSTNAME"] => stan.site5.com
+//$_SERVER["PWD"] => /home/pwnoogle/htdocs/emulive/htdocs
+//$_SERVER["DOCUMENT_ROOT"] => ''
 
 
-// compression 1/0
-$use_gzip = 0;
-// use memcache? 0 == off
-$memcache_expire = 0;
+function
+get_hostname ()
+{
+  // default
+  $p = 'tv2_domain';
+
+//  if (isset ($_SERVER['SERVER_NAME']))
+//    $p = $_SERVER['SERVER_NAME'];
+//  else if (isset ($_SERVER['HOSTNAME']))
+//    $p = $_SERVER['HOSTNAME'];
+//  else
+    $p = php_uname ('n');
+  return $p;
+//  return gethostname ();
+}
 
 
-// wget path and options
-$wget_path = '/usr/bin/torify /usr/bin/wget'; // use TOR for greedy RSS feeds
-// rsstool path and options
-$rsstool_path = '/usr/bin/torify /usr/local/bin/rsstool';
-$rsstool_opts = '--hack-google --sbin --shtml';
+function
+get_subdomain ()
+{
+  // default
+  $p = 'tv2_subdomain';
+
+  // apache2
+  if (isset ($_SERVER['SERVER_NAME']))
+    if (trim ($_SERVER['SERVER_NAME']) != '')
+    {
+      $p = substr ($_SERVER['SERVER_NAME'], 0, strpos ($_SERVER['SERVER_NAME'], '.'));
+      // DEBUG
+//      echo $p;
+      return $p;
+    }
+
+  // cmdline
+  $p = $_SERVER['DOCUMENT_ROOT'];
+  if (isset ($_SERVER['PWD']))
+    $p = $_SERVER['PWD'];
+  $a = explode ('/', $p); // /home/pwnoogle/htdocs/emulive/htdocs
+  // DEBUG
+//  print_r ($a);
+  if (isset ($a[count ($a) - 2]))
+    return $a[count ($a) - 2];
+  return '';
+}
 
 
-// configuration
-$rsscache_config_xml = 'config.xml';
-$rsscache_thumbnails_prefix = '';
-$rsscache_debug_sql = 0;
-$rsscache_enable_search = 1; // allow users to search
-$rsscache_related_search = 1; // make use of related searches (requires keywords)
-$rsscache_xsl_stylesheet = 'http://'.$_SERVER['SERVER_NAME'].'/pwnoogle.xsl';
-$rsscache_js = 'http://'.$_SERVER['SERVER_NAME'].'/pwnoogle.js';
+$tv2_domain = get_hostname ();
+$tv2_subdomain = get_subdomain ();
+// DEBUG
+//echo '$tv2_domain=='.$tv2_domain."<br>\n";
+//echo '$tv2_subdomain=='.$tv2_subdomain."<br>\n";
 
+// HACK
 
-// database settings
-$rsscache_dbhost = 'localhost';
-$rsscache_dbname = 'rsscache';
-$rsscache_dbuser = 'root';
-$rsscache_dbpass = 'nb';
+if (in_array ($tv2_subdomain, array ('', 'www', 'pwnoogle')))
+  $tv2_subdomain = 'videos';
+if (isset ($_SERVER['SERVER_NAME']))
+  if ($_SERVER['SERVER_NAME'] == 'pwnoogle.com')
+    $tv2_subdomain = 'videos';
 
+if (isset ($_SERVER['PWD']))
+  $pwd = $_SERVER['PWD'].'/../htdocs/'; 
+else  
+  $pwd = $_SERVER['DOCUMENT_ROOT'];
 
-// set user agent for downloads
-require_once ('misc/misc.php');
-ini_set('rsscache_user_agent', random_user_agent ());
-$rsscache_user_agent = random_user_agent ();
+if (!file_exists ($pwd.'/'.$tv2_subdomain.'_config.php'))
+  $tv2_subdomain = 'videos';
 
+require_once ($tv2_subdomain.'_config.php');
 
 }
 
---
+++ rsscache/htdocs/rsscache_config.php
@@ -0,0 +1,46 @@
+<?php
+if (!defined ('RSSCACHE_CONFIG_PHP'))
+{
+define ('RSSCACHE_CONFIG_PHP', 1);
+
+
+// compression 1/0
+$use_gzip = 0;
+// use memcache? 0 == off
+$memcache_expire = 0;
+
+
+// wget path and options
+$wget_path = '/usr/bin/torify /usr/bin/wget'; // use TOR for greedy RSS feeds
+// rsstool path and options
+$rsstool_path = '/usr/bin/torify /usr/local/bin/rsstool';
+$rsstool_opts = '--hack-google --sbin --shtml';
+
+
+// configuration
+$rsscache_config_xml = 'config.xml';
+$rsscache_thumbnails_prefix = '';
+$rsscache_debug_sql = 0;
+$rsscache_enable_search = 1; // allow users to search
+$rsscache_related_search = 1; // make use of related searches (requires keywords)
+$rsscache_xsl_stylesheet = 'http://'.$_SERVER['SERVER_NAME'].'/pwnoogle.xsl';
+$rsscache_js = 'http://'.$_SERVER['SERVER_NAME'].'/pwnoogle.js';
+
+
+// database settings
+$rsscache_dbhost = 'localhost';
+$rsscache_dbname = 'rsscache';
+$rsscache_dbuser = 'root';
+$rsscache_dbpass = 'nb';
+
+
+// set user agent for downloads
+require_once ('misc/misc.php');
+ini_set('rsscache_user_agent', random_user_agent ());
+$rsscache_user_agent = random_user_agent ();
+
+
+}
+
+
+?>
